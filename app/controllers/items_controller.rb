ed97670677eaed152c6a957051de661e4e9069a5
sorayuri <sirayurina0107@gmail.com>
2026-04-23 21:59:21 +0900

    ActiveStorageを導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+   def item_params
+     params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_burden_id, :prefecture_id, :shipping_day_id,:image).merge(user_id: current_user.id)
 end
