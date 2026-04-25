909d5d3f85cabb28eb069b3786f6c176439fde19
sorayuri <sirayurina0107@gmail.com>
2026-04-25 13:05:18 +0900

    単体テストコードの実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,11 +14,11 @@ class Item < ApplicationRecord
    validates :description, presence: true
    validates :image, presence: true
 
-   validates :category_id, numericality: { other_than: 1, message: "を選択してください" }
-   validates :condition_id, numericality: { other_than: 1, message: "を選択してください" }
-   validates :shipping_fee_id, numericality: { other_than: 1, message: "を選択してください" }
-   validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
-   validates :shipping_day_id, numericality: { other_than: 1, message: "を選択してください" }
+   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+   validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
 
    validates :price,
           presence: true,
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+
+     name { "テスト商品" }
+     description { "テスト説明" }
+     price { 1000 }
+
+     category_id { 2 }
+     condition_id { 2 }
+     shipping_fee_id { 2 }
+     prefecture_id { 2 }
+     shipping_day_id { 2 }
+
+     association :user
+
+     after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+     end
   end
 end
