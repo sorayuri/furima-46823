1a4fa66bf769ee3dbe4467252ac4d0a63d7d107e
sorayuri <sirayurina0107@gmail.com>
2026-05-01 02:47:42 +0900

    決済機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include Gon::ControllerHelpers
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,16 +4,18 @@ class OrdersController < ApplicationController
   before_action :move_to_index
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
      @order_address = OrderAddress.new
 end
 
   def create
     @order_address = OrderAddress.new(order_params)
-
     if @order_address.valid?
-      @order_address.save
-      redirect_to root_path
+        pay_item
+        @order_address.save
+       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -23,7 +25,18 @@ end
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :token).merge( user_id: current_user.id, item_id: params[:item_id])
   end
-  
+
+   def pay_item
+    Rails.logger.debug "SECRET_KEY: #{ENV['PAYJP_SECRET_KEY']}"
+
+       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+       Payjp::Charge.create(
+         amount: @item.price,
+         card: order_params[:token],
+         currency: 'jpy'
+       )
+  end
+
   def move_to_index
     if @item.order.present? || current_user.id == @item.user_id
       redirect_to root_path
